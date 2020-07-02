6cad228c67a7b590f11eae2610dbc336e11da8eb
nathan <nathankfoss@gmail.com>
2020-07-01 19:26:28 -0500

    Create signup method
    The secret file was also added to git-ignore

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as data from '../shared/credentials.json'
+
+interface AuthResponseData {
+  kind: string,
+  idToken: string,
+  email: string,
+  refreshToken: string,
+  expiresIn: string,
+  localId: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signup(email: string, password: string) {  
+    let secret = data.apiKey;
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=' + secret,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+  }
 }
