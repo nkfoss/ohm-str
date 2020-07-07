791a486462768896a2c5d2b50741a5acc280564b
nathan <nathankfoss@gmail.com>
2020-07-06 21:18:54 -0500

    Create route-guard for authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,20 @@ import { EditExerciseComponent } from './sets/edit-exercise/edit-exercise.compon
 import { SetListComponent } from './sets/setList/setList.component';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth/auth.component';
+import { AuthGuard } from './auth/auth/auth.guard';
 
 // ======================================================
 
 const appRoutes: Routes = [
 
-  {path: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
 
   {path: 'auth', component: AuthComponent},
 
-  {path: 'workout/:date', component: SetListComponent },
+  {path: 'workout/:date', component: SetListComponent, canActivate: [AuthGuard]},
 
   {path: 'exercise', 
+  canActivate: [AuthGuard],
   children: [
     {path: ':exerciseId/edit', component: EditExerciseComponent},
     {path: 'new', component: EditExerciseComponent}
