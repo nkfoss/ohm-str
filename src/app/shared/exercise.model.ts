98bc03c3788f94a0ea863f4812f01d233634df15
nathan <nathankfoss@gmail.com>
2020-07-18 23:41:09 -0500

    Add notes to exercise model

--- src/app/shared/exercise.model.ts
+++ src/app/shared/exercise.model.ts
@@ -5,6 +5,7 @@ export class Exercise {
     constructor(
         public exerciseName: string,
         public setType: string,
+        public notes: string,
         public warmupSets: WeightReps[],
         public sets: WeightReps[]
     ) { }
