e08f48030a2b2c980baa29e9897618b29218689f
nathan <nathankfoss@gmail.com>
2020-07-23 20:19:50 -0500

    Add dropSets to Set model
    Addresses #5

--- src/app/shared/weightReps.model.ts
+++ src/app/shared/weightReps.model.ts
@@ -2,5 +2,7 @@ export class WeightReps{
     constructor(
         public weight: number, 
         public reps: number,
-        public restPauseSets?: number[]) {}
+        public restPauseSets?: number[],
+        public dropSets?: {weight: number, reps: number}[]
+        ) {}
 }
