49e7103959480b31b7d0bd4bb69baf3df0627f47
nathan <nathankfoss@gmail.com>
2020-07-23 20:22:37 -0500

    Start implementing 1rm functionality
    Addresses #3

--- src/app/repmax.service.ts
+++ src/app/repmax.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Workout } from './shared/workout.model';
-import { Exercise } from './shared/exercise.model';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -8,12 +7,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RepmaxService {
 
+  recordMaxes: Object;
+
   constructor(private http: HttpClient) { }
 
   // ===============================================
 
   calculateMax(reps: number, weight: number) {
-    let unrounded = weight * (1 + (reps/30));
+    let unrounded = weight * (1 + (reps / 30));
     return +unrounded.toFixed(2)
   }
 
@@ -21,8 +22,8 @@ export class RepmaxService {
 
     workout.exercises.forEach(exercise => {
 
-      if (exercise.sets.length < 1) { return; } // This is essentially 'continue'... to skip iterations with no sets.
-      
+      if (exercise.sets.length < 1 || exercise.setType === "clusters") { return; } // This is essentially 'continue'... to skip iterations with no sets.
+
       let maxArray = [];
       exercise.sets.forEach(set => {
         maxArray.push(this.calculateMax(set.reps, set.weight));
@@ -34,11 +35,29 @@ export class RepmaxService {
         ORM: calculatedMax
       }
 
-      const url = 'https://strengthpractice-7e443.firebaseio.com/repmax/' 
-      + exercise.exerciseName.toLowerCase() + '/' + entry.date + '.json'
+      const url = 'https://strengthpractice-7e443.firebaseio.com/daymaxes/'
+        + exercise.exerciseName.toLowerCase() + '/' + entry.date + '.json'
       this.http.patch(url, entry).subscribe(response => { console.log(response) })
-      }
+    }
     )
+  }
+
+  fetchRecords() {
+    let url = 'https://strengthpractice-7e443.firebaseio.com/recordmaxes.json'
+    this.http.get(url).subscribe(response => {
+      this.recordMaxes = response;
+      console.log(this.recordMaxes)
+    } )
+  }
+
+  getNames(obj, name) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (key == name) {
+          console.log(obj[key])
+        }
+      }
+    }
   }
 
 }
