a87d31400ebb86fcf62ce3f822c39cf399c3be49
nathan <nathankfoss@gmail.com>
2020-08-04 19:36:17 -0500

    Add percentEffort to Exercise object

--- src/app/shared/weightReps.model.ts
+++ src/app/shared/weightReps.model.ts
@@ -2,6 +2,7 @@ export class WeightReps{
     constructor(
         public weight: number, 
         public reps: number,
+        public percentEffort?: number,
         public restPauseSets?: number[],
         public dropSets?: {weight: number, reps: number}[]
         ) {}
