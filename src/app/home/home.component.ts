b7933d47f01f7c4e2f2a6ca89102e3576b346629
nathan <nathankfoss@gmail.com>
2020-08-18 20:20:30 -0500

    Bind component property to date picker selection
    This had to be done a custom way, as the value property did not work. There is a comment explaining what was done.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StringHandlerService } from '../string-handler.service';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,8 @@ import { StringHandlerService } from '../string-handler.service';
 // ==================================================
 
 export class HomeComponent {
+
+  selectedDate: Date;
 
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
@@ -25,6 +28,15 @@ export class HomeComponent {
     )
     const toNavigate = 'workout/' + today;
     this.router.navigate([toNavigate])
+  }
+
+  setSelectedDate(event: MatDatepickerInputEvent<Date>) {
+    this.selectedDate = event.value;
+    console.log(event.value)
+  }
+
+  printDate() {
+    console.log( this.selectedDate.toDateString() )
   }
 
 }
