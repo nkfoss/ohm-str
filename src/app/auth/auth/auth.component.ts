0f949e61a41eca401b1d9d58002c4c3b6f94f56d
nathan <nathankfoss@gmail.com>
2020-08-20 20:57:56 -0500

    Remove sign-up and materialize auth page
    closes #23
    closes #24

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class AuthComponent implements OnInit {
 
-  isLoginMode = true;
+  private isLoginMode = true;
   isLoading = false;
   error: string = null;
 
