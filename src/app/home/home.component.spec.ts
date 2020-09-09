9f0dde0631b576da8319931676dd6d113ed0f933
michael-small <small203@morris.umn.edu>
2020-09-08 19:08:59 -0500

    Upload WIP date selector test
    The expectation should be that string but it expects `Expected<void>'`

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -4,6 +4,8 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientModule } from "@angular/common/http";
 
 describe("HomeComponent", () => {
+  let home: HomeComponent;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
@@ -17,4 +19,13 @@ describe("HomeComponent", () => {
     const home = fixture.debugElement.componentInstance; // gets the actual component (aka the app itself)
     expect(home).toBeTruthy();
   }));
+
+  // it("sets selected date", async () => {
+  //   const date: any = { value: new Date(2017, 0, 1) };
+  //   const value = home.setSelectedDate(date);
+  //   console.log(value);
+  //   expect(value).toEqual(
+  //     "Tue Sep 08 2020 00:00:00 GMT-0500 (Central Daylight Time)"
+  //   );
+  // });
 });
