2eb8f4e7e52226a9f841c459f6d431b5b6354e2c
michael-small <small203@morris.umn.edu>
2020-09-11 14:24:40 -0500

    Comment out WIP `home.component.spec.ts`, simplicy creation test
    I have gone out of the scope of my intentions on this branch. I
    just wanted boilerplate test files and got stuck on the date picker
    test. I will add boilerplate for other components first.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,10 +1,11 @@
-import { TestBed, async } from "@angular/core/testing";
+import { ComponentFixture, TestBed, async } from "@angular/core/testing";
 import { HomeComponent } from "./home.component";
 import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientModule } from "@angular/common/http";
 
 describe("HomeComponent", () => {
   let home: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,16 +15,24 @@ describe("HomeComponent", () => {
     }).compileComponents();
   });
 
+  beforeEach(async(() => {
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(HomeComponent);
+      home = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
+
   it("should be created", async(() => {
-    const fixture = TestBed.createComponent(HomeComponent); // 'fixture' is a common name for the comp
-    const home = fixture.debugElement.componentInstance; // gets the actual component (aka the app itself)
     expect(home).toBeTruthy();
   }));
 
   // it("sets selected date", async () => {
   //   const date: any = { value: new Date(2017, 0, 1) };
+  //   const fixture = TestBed.createComponent(HomeComponent); // 'fixture' is a common name for the comp
+  //   const home = fixture.debugElement.componentInstance; // gets the actual component (aka the app itself)
   //   const value = home.setSelectedDate(date);
-  //   console.log(value);
+  //   console.log("value: " + value);
   //   expect(value).toEqual(
   //     "Tue Sep 08 2020 00:00:00 GMT-0500 (Central Daylight Time)"
   //   );
