83ae6b6003d57c0828c563d95916f533ae09cd25
nathan <nathankfoss@gmail.com>
2020-11-10 19:51:40 -0600

    Implement new today navigation, and documenation

--- src/app/header/navbar.component.ts
+++ src/app/header/navbar.component.ts
@@ -16,9 +16,21 @@ import { RepmaxService } from "../repmax.service";
 
 export class NavbarComponent implements OnInit, OnDestroy {
 
-	isAuthenticated = false; // Authenticated users see different things.
+	/**
+	 * The user's authentication status. Unauthenticated users can only interact with the login page.
+	 * Automatically updated by the userSub.
+	 */
+	isAuthenticated = false; 
+
+	/**
+	 * Used to update isAuthenticated. 
+	 * If no User object is received, then the current user is unauthenticated.
+	 */
 	private userSub: Subscription; // also used for authentication.
 
+	/**
+	* Used to open the navbar element. Toggled when the user clicks on the navbar.
+	*/
 	isNavbarCollapsed = true; 
 
 	// =====================================================
@@ -39,34 +51,47 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
 	// =====================================================
 
+	/**
+	 * Button function. Tells the AuthService to logout.
+	 */
 	onLogout() {
 		this.authService.logout()
 	}
 
+	/**
+	 * Button function. Navigates to the home page.
+	 */
 	onNavigateHome() {
 		this.router.navigate(['home']);
 		this.isNavbarCollapsed = !this.isNavbarCollapsed;
 	}
 
-	onFetchData() {
-		const date = new Date().toDateString();
-		this.workoutService.fetchWorkout(date);
-	}
 
+	// onFetchData() {
+	// 	const date = new Date().toDateString();
+	// 	this.workoutService.fetchWorkout(date);
+	// }
+
+	/**
+	 * Button function. Navigates to the current date.
+	 */
 	onNavigateToToday() {
-		const today = this.stripWeekday(
-			new Date().toDateString()
-		)
+		const today = this.stripWeekday( new Date().toDateString() )
 		const toNavigate = 'workout/' + today;
 
 		this.router.navigate([toNavigate]);
 		this.isNavbarCollapsed = !this.isNavbarCollapsed;
-		if (this.workoutService.workout.date !== today) {
-			this.workoutService.fetchWorkout(today);
-		}
+		// if (this.workoutService.workout.date !== today) {
+		// 	this.workoutService.fetchWorkout(today);
+		// }
 	}
 
-	stripWeekday(dateString: string) {
+	/**
+	 * Remove the weekday from a Date object that was converted to a string.
+	 * @param dateString - A date object converted to a string.
+	 * @returns A correctly formatted date-string to be used in navigation.
+	 */
+	private stripWeekday(dateString: string): string {
 		const regex = /^.{4}/gi
 		return dateString.replace(regex, '')
 	}
