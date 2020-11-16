ab92c0cb36600d082d47581c1f78040c87cc5d4f
nathan <nathankfoss@gmail.com>
2020-11-16 17:25:01 -0600

    Add mock login the AuthService Stub

--- src/app/shared/mocks/auth.service.mock.ts
+++ src/app/shared/mocks/auth.service.mock.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
 import { User } from "src/app/auth/user.model";
 
 export class AuthServiceStub {
@@ -6,5 +6,30 @@ export class AuthServiceStub {
 
 	logout() {
 		this.userSubject.next(null);
+	}
+
+	/**
+	 * Asynchronous. Uses an observable w/ 1-second timeout to mock logging in.
+	 * Correct email/password returns AuthResponse object. Incorrect returns an Error object.
+	 * @param email  
+	 * @param password 
+	 */
+	login(email: string, password: string) {
+
+		return Observable.create(observer => {
+			setTimeout(() => {
+				if ((email == 'jacob@email.com') && (password == 'qwertyuiop')) {
+					observer.next({
+						kind: "kind value",
+						idToken: "idToken value",
+						email: email,
+						refreshToken: "refreshToken value",
+						expiresIn: "expiresIn value",
+						localId: "localId value",
+					});
+				}
+				else { observer.error('An error occurred.'); }
+			}, 1000);
+		});
 	}
 }
