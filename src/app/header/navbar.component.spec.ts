be50387462bb6a688328f166233214399fb31fd8
nathan <nathankfoss@gmail.com>
2020-11-16 11:45:10 -0600

    Add navigation test to Navbar Component test

--- src/app/header/navbar.component.spec.ts
+++ src/app/header/navbar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AuthService } from "../auth/auth.service";
 import { NavbarComponent } from "./navbar.component"
@@ -6,6 +7,7 @@ import { NavbarComponent } from "./navbar.component"
 describe("navbar", () => {
 	let navbar: NavbarComponent;
 	let fixture: ComponentFixture<NavbarComponent>;
+	let router: Router;
 
 	let authServiceStub: {};
 
@@ -19,11 +21,29 @@ describe("navbar", () => {
 				{ provide: AuthService, useValue: authServiceStub }
 			]
 		});
+
+		fixture = TestBed.createComponent(NavbarComponent);
+		navbar = fixture.componentInstance;
+		router = TestBed.inject(Router);
+
+		fixture.detectChanges();
 	});
 
 	it('should create the Navbar Component', () => {
-		let fixture = TestBed.createComponent(NavbarComponent);
-		let NC = fixture.debugElement.componentInstance;
-		expect(NC).toBeTruthy();
+		expect(navbar).toBeTruthy();
+	})
+
+	it('should navigate to homepage', () => {
+		const navigateSpy = spyOn(router, 'navigate');
+		navbar.onNavigateHome();
+		expect(navigateSpy).toHaveBeenCalledWith(['home']);
+	})
+
+	it('should navigate to the current day', () => {
+		const navigateSpy = spyOn(router, 'navigate');
+
+		const today = navbar.formatDatePipe.transform(new Date().toDateString(), 'LLL d y');
+		navbar.onNavigateToToday();
+		expect(navigateSpy).toHaveBeenCalledWith(['workout/' + today])
 	})
 })
