09afc052b249e02dc57bd9fce20efda2caed89c6
nathan <nathankfoss@gmail.com>
2020-11-15 23:15:50 -0600

    Add tests for navigation in Home Component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,12 +1,14 @@
+import { BoundElementProperty } from "@angular/compiler";
 import { DebugElement } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
 import { RouterTestingModule } from "@angular/router/testing";
 import { HomeComponent } from "./home.component"
 
 describe("home", () => {
 	let home: HomeComponent;
 	let fixture: ComponentFixture<HomeComponent>;
-	let de: DebugElement;
+	let router: Router;
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
@@ -17,13 +19,31 @@ describe("home", () => {
 		});
 
 		fixture = TestBed.createComponent(HomeComponent);
-		de = fixture.debugElement;
-		home = de.componentInstance
-
+		home = fixture.componentInstance
+		router = TestBed.inject(Router);
+		
 		fixture.detectChanges();
 	});
 
 	it('should create the Home Component', () => {
 		expect(home).toBeTruthy();
 	})
+
+	it('should navigate to the current day', () => {
+		const navigateSpy = spyOn(router, 'navigate');
+		
+		const today = home.formatDatePipe.transform(new Date().toDateString(), 'LLL d y');
+		home.getTodaysWorkout();
+		expect(navigateSpy).toHaveBeenCalledWith(['workout/' + today])
+	});
+	
+	it('should navigate to a given date', () => {
+		const navigateSpy = spyOn(router, 'navigate');
+
+		home.formattedDate = "Nov 5 2020";
+		home.navigateToDate();
+		expect(navigateSpy).toHaveBeenCalledWith(['workout/Nov 5 2020'])
+	})
+
+
 })
