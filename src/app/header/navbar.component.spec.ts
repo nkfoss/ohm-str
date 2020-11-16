bce54b7d2c475db6b1bd10e0e2f60da6b51659a7
nathan <nathankfoss@gmail.com>
2020-11-16 14:37:25 -0600

    Finish Navbar Component unit tests

--- src/app/header/navbar.component.spec.ts
+++ src/app/header/navbar.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { Router } from "@angular/router";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AuthService } from "../auth/auth.service";
+import { AuthServiceStub } from "../shared/mocks/auth.service.mock";
 import { NavbarComponent } from "./navbar.component"
 
 describe("navbar", () => {
@@ -9,28 +10,36 @@ describe("navbar", () => {
 	let fixture: ComponentFixture<NavbarComponent>;
 	let router: Router;
 
-	let authServiceStub: {};
+	// let authServiceStub: { 
+	// 	userSub: Subscription;
+	// };
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			imports: [
-				RouterTestingModule
-			],
+			imports: [RouterTestingModule],
 			declarations: [NavbarComponent],
-			providers: [
-				{ provide: AuthService, useValue: authServiceStub }
-			]
+			providers: [{ provide: AuthService, useClass: AuthServiceStub }]
 		});
 
-		fixture = TestBed.createComponent(NavbarComponent);
-		navbar = fixture.componentInstance;
 		router = TestBed.inject(Router);
-
+		fixture = TestBed.createComponent(NavbarComponent);
+		navbar = fixture.debugElement.componentInstance;
 		fixture.detectChanges();
+
 	});
 
 	it('should create the Navbar Component', () => {
 		expect(navbar).toBeTruthy();
+	})
+
+	it('should not be initially authenticated', () => {
+		expect(navbar.isAuthenticated).toBe(false);
+	})
+
+	it('should not be authenticated after logout', () => {
+		navbar.isAuthenticated = true;
+		navbar.onLogout();
+		expect(navbar.isAuthenticated).toBe(false);
 	})
 
 	it('should navigate to homepage', () => {
