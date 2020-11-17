e487b1057fc0c1c7b68ab6e7b463f6a7b395fa46
nathan <nathankfoss@gmail.com>
2020-11-17 14:25:47 -0600

    Create tests for SetList Component onInit()

--- src/app/sets/setList/setList.component.spec.ts
+++ src/app/sets/setList/setList.component.spec.ts
@@ -1,21 +1,21 @@
 import { ComponentFixture, TestBed, async } from "@angular/core/testing";
-import { By } from "@angular/platform-browser";
-import { DebugElement } from "@angular/core";
-
 import { SetListComponent } from "./setList.component";
 import { WorkoutService } from "../../workout.service";
 import { RepmaxService } from "src/app/repmax.service";
 import { RouterTestingModule } from "@angular/router/testing";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { WorkoutServiceStub } from "src/app/shared/mocks/workout.service.mock";
+import { RepmaxServiceStub } from "src/app/shared/mocks/repmax.service.mock";
+import { ActivatedRoute, Router } from "@angular/router";
+import { of } from "rxjs";
 
 describe("setList", () => {
   let setList: SetListComponent;
   let fixture: ComponentFixture<SetListComponent>;
+  let router: Router;
+  let route: ActivatedRoute;
 
-  let workoutServiceStub: {};
-  let repmaxServiceStub: {};
-
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -23,19 +23,81 @@ describe("setList", () => {
       ],
       declarations: [SetListComponent],
       providers: [
-        { provide: WorkoutService, useValue: workoutServiceStub },
-        { provide: RepmaxService, useValue: repmaxServiceStub },
+        { provide: WorkoutService, useClass: WorkoutServiceStub },
+        { provide: RepmaxService, useClass: RepmaxServiceStub },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: { date: 'Nov 05 2020' }
+            },
+            params: of({ date: 'Nov 05 2020' })
+          }
+        }
       ]
     });
-  });
+
+    router = TestBed.inject(Router);
+    route = TestBed.inject(ActivatedRoute);
+    fixture = TestBed.createComponent(SetListComponent);
+    setList = fixture.debugElement.componentInstance;
+
+
+    // let workoutService = fixture.debugElement.injector.get(WorkoutService);
+
+    fixture.detectChanges();
+  }));
 
   it('should create the SetList Component', () => {
-    let fixture = TestBed.createComponent(SetListComponent);
-    let SLC = fixture.debugElement.componentInstance;
-    expect(SLC).toBeTruthy();
-  })
+    expect(setList).toBeTruthy();
+  });
+
+  it('should setup the exercise and bodyweight subs', () => {
+    expect(setList.bodyweightSub).toBeDefined();
+    expect(setList.exerciseSub).toBeDefined();
+  });
+
+  it('should indicate that a workout was not initially found', () => {
+    let consoleSpy = spyOn(console, 'log');
+    let workoutService = fixture.debugElement.injector.get(WorkoutService);
+    workoutService.workout = null;
+    setList.ngOnInit();
+    expect(consoleSpy).toHaveBeenCalledWith("No workout found");
+  });
+
+  it('should have the exercise array and bodyweight updated', () => {
+    expect(setList.exercises).toEqual([
+      {
+        exerciseName: 'Bench Press',
+        setType: 'rpd',
+        exerciseNotes: 'bench press notes',
+        momentaryMax: 200,
+        warmupSets: [],
+        sets: [{ weight: 200, reps: 1 }]
+      },
+      {
+        exerciseName: 'Squat',
+        setType: 'mtor',
+        exerciseNotes: 'squat notes',
+        momentaryMax: 275,
+        warmupSets: [],
+        sets: [{ weight: 275, reps: 1 }]
+      },
+      {
+        exerciseName: 'Deadlift',
+        setType: 'clusters',
+        exerciseNotes: 'deadlift notes',
+        momentaryMax: 300,
+        warmupSets: [],
+        sets: [{ weight: 300, reps: 1 }]
+      },
+    ])
+    expect(setList.bodyweight).toEqual(170);
+  });
+
+  it('should have the date property match the service\'s workout date', () => {
+    let workoutService = fixture.debugElement.injector.get(WorkoutService);
+    expect(setList.date).toEqual(workoutService.workout.date);
+  });
 
 });
-
-// activated route: {"_isScalar":false,"observers":[],"closed":false,"isStopped":false,"hasError":false,"thrownError":null,"_value":[{"path":"workout","parameters":{}},{"path":"Sep 04 2020","parameters":{}}]}
-// URL: workout,Sep%2004%20202
