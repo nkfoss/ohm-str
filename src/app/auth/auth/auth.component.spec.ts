c5b4cbb80d5e0fcd610cf387baf2833d88ad2f41
nathan <nathankfoss@gmail.com>
2020-11-24 08:40:44 -0600

    Add import to remedy occasional error

--- src/app/auth/auth/auth.component.spec.ts
+++ src/app/auth/auth/auth.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { Router } from "@angular/router";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AuthServiceStub } from "src/app/shared/mocks/auth.service.mock";
@@ -14,7 +15,8 @@ describe("auth", () => {
 		TestBed.configureTestingModule({
 			imports: [
 				RouterTestingModule,
-				ReactiveFormsModule
+				ReactiveFormsModule,
+				BrowserAnimationsModule
 			],
 			declarations: [AuthComponent],
 			providers: [
